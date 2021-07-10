3de8ea309a4ea1a4f1c148bed6e7c33f5c87f907
Andrei Radovici <andrei.iulian.radovici@gmail.com>
2021-07-10 17:37:25 +0100

    globalfunctions file created

--- includes/globalfunctions.php
+++ includes/globalfunctions.php
@@ -1,10 +1,21 @@
 <?php
-
+// Used to insert mustache templates
 function getTemplate(string $path): string 
 {
     ob_start();
     require_once $path;
     return ob_get_clean();
+}
+// Used to redirect to other page
+function redirectTo(string $url): void 
+{
+    header('Location: ' . $url);
+    exit();
+}
+// Used to debug code
+function debug($variable): string 
+{
+    return highlight_string("<?php\n\$data =\n" . var_export($variable, true) . ";\n?>");
 }
 
 ?>
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'includes/globalfunctions.php';
 $template = getTemplate('mustacheTemplates/index.mst');
-
+debug($_SERVER);
 ?>
 
 <!-- Header -->
@@ -9,6 +9,7 @@ $template = getTemplate('mustacheTemplates/index.mst');
 <!-- Style -->
 <?php require_once 'styles/index.html'; ?>
 <!-- Template -->
+<h1>This page is just a test</h1>
 <?= $mustache->render($template, ['planet' => 'World!']);  ?>
 <!-- Footer -->
 <?php require_once 'includes/mainfooter.php'; ?>
