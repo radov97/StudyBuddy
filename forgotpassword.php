f9c88851b2294bcb2ec7341e42e3075d906b805c
Andrei Radovici <andrei.iulian.radovici@gmail.com>
2021-07-16 12:31:07 +0100

    Forgotten password

--- forgotpassword.php
+++ forgotpassword.php
@@ -1,6 +1,26 @@
 <?php
 require_once 'includes/globalfunctions.php';
 $forgotPassword = getTemplate('mustacheTemplates/forgotpassword.mst');
+if (isset($_POST['resend_password'])) {
+    $safeEmail = trimInputSides($_POST['email']);
+    $isUserValid = checkUserAccount($safeEmail);
+    do {
+        // Invalid user
+        if (empty($isUserValid)) {
+            $errorMessage = 'Invalid email address.';
+            unset($successMessage);
+            break;
+        }
+        $message = 'Your password is: ' . $isUserValid['password'];
+        if (!mail($safeEmail, 'Forgotten Password', $message, 'From: ' . BASE_DOMAIN_EMAIL)) {
+            $errorMessage = 'Something went wrong. Could not send your password. Please try again later.';
+            unset($successMessage);
+            break;
+        }
+        $successMessage = 'An email containing your password has been successfully sent.';
+        unset($errorMessage);
+    } while(0);
+}
 
 ?>
 
