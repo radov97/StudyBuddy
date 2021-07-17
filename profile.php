e90138ed86d883cfb202dc907efa35ba165086dc
Andrei Radovici <andrei.iulian.radovici@gmail.com>
2021-07-17 11:09:34 +0100

    keep scrolling but hide scrollbar

--- profile.php
+++ profile.php
@@ -11,10 +11,20 @@ if (!isset($_SESSION['user_logged'])) {
 <!-- Header -->
 <?php require_once 'includes/mainheader.php'; ?>
 <!-- Style -->
+<style>
 
+
+</style>
 <!-- Template -->
 
-<h1 class="bg-warning">Profile</h1>
+<div class="container bg-danger test">
+    <h1>
+    t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+    </h1>
+    <h1>
+    t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+    </h1>
+</div>
 
 <!-- Footer -->
 <?php require_once 'includes/mainfooter.php'; ?>
