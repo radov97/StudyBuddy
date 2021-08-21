9eda044dd9cb1fd0ef75ee7d6a3b2f4cc70f53a2
Andrei Radovici <andrei.iulian.radovici@gmail.com>
2021-08-21 12:30:07 +0100

    Reset Password work in progress

--- resetpassword.php
+++ resetpassword.php
@@ -1,11 +1,18 @@
 <?php
 require_once 'includes/globalfunctions.php';
 $resetPassword = getTemplate('mustacheTemplates/resetpassword.mst');
-
 // Set mustache data
-$resetPasswordData = [
-    'login_url' => BASE_DOMAIN_URL . 'login.php',
-];
+$resetPasswordData = ['login_url' => BASE_DOMAIN_URL . 'login.php'];
+
+debug($_POST);
+if (isset($_POST['reset_password'])) {
+    $safePassword = trimInputSides($_POST['password']);
+    $confirmPassword = trimInputSides($_POST['confirm_password']);
+    // Password doesn't match requirement criteria
+    // Password is not the same
+    // Something went wrong
+    // Update new password
+}
 ?>
 
 <!-- Header -->
